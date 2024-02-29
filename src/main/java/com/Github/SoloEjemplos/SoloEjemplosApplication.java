1f0158c32551689977d190ffd87d7925bf7c3d8e
Abel F <fuciliabel@gmail.com>
2024-02-29 20:36:24 -0300

    Agregamos el proyecto

---
+++ src/main/java/com/Github/SoloEjemplos/SoloEjemplosApplication.java
@@ -0,0 +1,13 @@
+package com.Github.SoloEjemplos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloEjemplosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloEjemplosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Github/SoloEjemplos/SoloEjemplosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Github.SoloEjemplos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloEjemplosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
